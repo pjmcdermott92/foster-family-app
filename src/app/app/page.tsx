59cf05c7dedcf02d4d9c1ec9eb239cf7a6f212a8
pjmcdermott92 <patrick@pjmcdermott.com>
2026-01-12 19:16:19 -0700

    add design tokens for theming

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 import { getCurrentUser } from "@/services/supabase/lib/getCurrentUser";
 
 export default async function AppPage() {
     const user = await getCurrentUser();
     console.log(user)
 
-    return <>APP PAGE</>
+    return <>APP PAGE <Button>Button</Button></>
 }
